7fccfe596d42e6a69a677a0585b560e74a719438
goziyev <goziyevmuhammadgozi@gmail.com>
2024-03-06 14:47:15 +0500

    add folder

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect } from "react";
+import dragula from "dragula";
+import './App.css'
+function App() {
+  useEffect(() => {
+    const drake = dragula([
+      document.getElementById("b1"),
+      document.getElementById("b2"),
+      document.getElementById("b3"),
+    ]);
+
+    // Scrollable area
+    const element = document.getElementById("boards");
+    const numberOfBoards = element.getElementsByClassName("board").length;
+    const boardsWidth = numberOfBoards * 316; // Width of all Boards
+    element.style.width = boardsWidth + "px"; // set Width
+
+    // disable text-selection
+    function disableselect(e) {
+      return false;
+    }
+    document.onselectstart = new Function();
+    document.onmousedown = disableselect;
+
+    return () => {
+      drake.destroy();
+      document.onselectstart = null;
+      document.onmousedown = null;
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>a trello'esque experiment</h1>
+      <p>
+        made by Toto <a href="https://twitter.com/herr_toto">@herr_toto</a> -
+        powered by <a href="https://bevacqua.github.io/dragula/">dragula</a>
+      </p>
+      <div id="kaban">
+        <div id="scroller">
+          <div id="boards">
+            <div className="board" id="board1">
+              <header>On hold</header>
+              <div className="cards" id="b1">
+                <div className="card">
+                  <span className="cardtitle noselect">A great card #1</span>
+                </div>
+                <div className="card">
+                  <span className="cardtitle noselect">A great card #2</span>
+                </div>
+                <div className="card">
+                  <span className="cardtitle noselect">A great card #3</span>
+                </div>
+                <div className="card">
+                  <span className="cardtitle noselect">A great card #4</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="board" id="board2">
+              <header>In Progress</header>
+              <div className="cards" id="b2">
+                <div className="card">
+                  <span className="cardtitle">A great card #5</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="board" id="board3">
+              <header>Done</header>
+              <div className="cards" id="b3">
+                <div className="card">
+                  <span className="cardtitle noselect">A great card #5</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
